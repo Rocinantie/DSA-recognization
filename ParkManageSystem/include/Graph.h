041c4c19a1710e4c573d5f4d7db89a57f3b2f4b4
rocinantie <weihnachtsbogen@gmail.com>
2019-05-13 19:50:46 +0800

    BasedOnStyle: Google, IndentWidth: 4

--- ParkManageSystem/include/Graph.h
+++ ParkManageSystem/include/Graph.h
@@ -5,58 +5,54 @@
 #pragma once
 
 // 定义顶点结构体
-struct Vex
-{
-	int num;
-	char name[20];
-	char description[1024];
+struct Vex {
+    int num;
+    char name[20];
+    char description[1024];
 };
 
 // 定义边结构体
-struct Edge
-{
-	int vex1;
-	int vex2;
-	int weight;
+struct Edge {
+    int vex1;
+    int vex2;
+    int weight;
 };
 
 // 定义导航路径结构体
-typedef struct Path
-{
-	int vexs[10];
-	Path *pNext;
+typedef struct Path {
+    int vexs[10];
+    Path *pNext;
 } * pathList;
 
 // 定义图结构体
-class Graph
-{
-private:
-	// 邻接矩阵
-	int ajdMatrix[20][20];
-	// 顶点
-	Vex vexs[10];
-	//顶点数目
-	int vexNum;
+class Graph {
+   private:
+    // 邻接矩阵
+    int ajdMatrix[20][20];
+    // 顶点
+    Vex vexs[10];
+    //顶点数目
+    int vexNum;
 
-public:
-	// 初始化
-	void init();
-	// 初始化图
-	void initialize();
-	// 打印图的信息
-	void print();
-	// 返回图的顶点
-	int getVexNumber();
-	// 返回顶点结构体
-	Vex getVex(int v);
-	// 查找邻接边
-	int findEdge(int v);
-	// 深度优先
-	void DFS(int nVex, bool bVisited[], int &index, pathList &pList);
-	// 深度优先遍历图
-	void DFSTraverse(int nVex, pathList &pList);
-	// 搜索最短路径
-	int findShortestPath(int vexStart, int vexEnd, Edge path[]);
-	// 最小生成树
-	void findMinTree(Edge path[]);
+   public:
+    // 初始化
+    void init();
+    // 初始化图
+    void initialize();
+    // 打印图的信息
+    void print();
+    // 返回图的顶点
+    int getVexNumber();
+    // 返回顶点结构体
+    Vex getVex(int v);
+    // 查找邻接边
+    int findEdge(int v);
+    // 深度优先
+    void DFS(int nVex, bool bVisited[], int &index, pathList &pList);
+    // 深度优先遍历图
+    void DFSTraverse(int nVex, pathList &pList);
+    // 搜索最短路径
+    int findShortestPath(int vexStart, int vexEnd, Edge path[]);
+    // 最小生成树
+    void findMinTree(Edge path[]);
 };
